f9941cf4776555ac03913d9e20de5fcd3c1fb29f
churtadov <carohurtado2010@hotmail.com>
2023-11-25 13:33:14 -0600

    api complete w images test

--- APIMinima/TodoDb.cs
+++ APIMinima/TodoDb.cs
@@ -1,6 +1,14 @@
-﻿namespace APIMinima
+﻿using Microsoft.EntityFrameworkCore;
+namespace APIMinima
 {
-    public class TodoDb
+    public class TodoDb : DbContext
     {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options)        
+        { 
+        
+        }
+
+        public DbSet<Todo> Todos => Set<Todo>();
     }
 }
